a233bf9ad6a53ccc8e1177f86a62542c42f694e1
joyanta1000 <joyanta955@gmail.com>
2020-06-12 02:51:11 +0600

    removed fie

--- Resources/Includes/footer.php
+++ Resources/Includes/footer.php
@@ -1,7 +1,7 @@
 <div class="card">
   <div class="card-body">
 
-<?php include 'index.php'; ?>
+
 
     <strong>&copy; <?php echo $CompanyName; ?> <?php echo date('Y');?></strong>	This is some text within a card body.
   </div>
